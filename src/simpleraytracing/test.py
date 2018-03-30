571cf6c2d56826e16021d229ffcc3222c31b19fa
Adriano Gil <adrianogil.san@gmail.com>
2018-03-29 21:19:20 -0400

    Define path when running in Termux

--- src/simpleraytracing/test.py
+++ src/simpleraytracing/test.py
@@ -5,6 +5,7 @@ from camera import Camera
 from vector import Vector3
 
 import random
+import os
 
 try:
     range = xrange
@@ -17,18 +18,24 @@ scene_test = Scene()
 
 for i in range(0, total_sphere):
     s = Sphere(random.randint(10,100) / 100.0)
-    s.transform.position = Vector3(random.randint(-3,3),random.randint(-3,3),random.randint(2,10))
-    s.albedo = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    s.transform.position = Vector3(random.randint(-2,2),random.randint(-2,2),random.randint(2,10))
+    s.albedo = (random.randint(20,255), random.randint(20,255), random.randint(20,255))
     scene_test.add_objects(s)
 
 # s2 = Sphere(1.6)
 # s2.transform.position = Vector3(0,1,4)
 # s2.albedo = (0,0,255)
 # scene_test.add_objects(s2)
-# scene_test.add_objects(s1)
 
 c = Camera()
 c.fov = 90
 
 scene_test.set_camera(c)
-scene_test.render(250,250, 'test.jpg')
+
+if os.path.exists("/sdcard/Raytracing/"):
+    render_image = "/sdcard/Raytracing/test.jpg"
+else:
+    render_image = 'test.jpg'
+
+scene_test.render(125,125, render_image)
+print('Scene rasterized in image path: %s' % (render_image,))
