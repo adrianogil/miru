52eb34e4bd8ed22316a9421449bf4a23a2d6ba1b
Adriano Gil <adrianogil.san@gmail.com>
2018-12-25 19:30:51 -0400

    Small test using png++

---
+++ src/cpp/image.cpp
@@ -0,0 +1,18 @@
+#include <string> 
+#include <png++/png.hpp>
+
+
+int main()
+{
+    //... Added sample from http://www.nongnu.org/pngpp/doc/0.2.9/
+    png::image< png::rgb_pixel > image(128, 128);
+    for (png::uint_32 y = 0; y < image.get_height(); ++y)
+    {
+        for (png::uint_32 x = 0; x < image.get_width(); ++x)
+        {
+            image[y][x] = png::rgb_pixel(x, y, x + y);
+            // non-checking equivalent of image.set_pixel(x, y, ...);
+        }
+    }
+    image.write("rgb.png");
+}
