2928adf05c9878ae67d87f24c23b3c63e91c9a6f
Adriano Gil <adrianogil.san@gmail.com>
2019-07-23 21:07:55 -0400

    [RaytracingInOneWeekend] Added WIP vec3 implementation

--- src/cpp/miru/oneweekend/test_ppm.cpp
+++ src/cpp/miru/oneweekend/test_ppm.cpp
@@ -1,4 +1,5 @@
-# include <iostream>
+#include <iostream>
+#include <vec3.h>
 
 int main()
 {
@@ -10,13 +11,11 @@ int main()
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++)
         {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
 
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
---
+++ src/cpp/miru/oneweekend/vec3.h
@@ -0,0 +1,67 @@
+#ifndef VEC3H
+#define VEC3H
+
+#include <math.h>
+#include "stdlib.h"
+#include "iostream"
+
+class vec3 {
+
+public:
+    vec3() {}
+    vec3(float e0, float e1 , float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+    inline float x() const { return e[0]; }
+    inline float y() const { return e[1]; }
+    inline float z() const { return e[2]; }
+    inline float r() const { return e[0]; }
+    inline float g() const { return e[1]; }
+    inline float b() const { return e[2]; }
+
+    inline const vec3& operator+() const { return *this; }
+    inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    inline float operator[](int i) const { return e[i]; }
+    inline float& operator[](int i) { return e[i]; };
+
+    inline vec3& operator/=(const float t);
+
+    inline float length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
+    inline float squared_length() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+
+    inline float dot(const vec3 &v1, const vec3 &v2) {
+        return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v1.e[2];
+    }
+
+    inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+        return vec3(
+                v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
+               -(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0]),
+               v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]
+            );
+    }
+
+    inline vec3 unit_vector(vec3 v) {
+        float len = v.length();
+        return vec3(v.e[0] / len, v.e[1] / len, v.e[2] / len);
+    }
+
+    float e[3];
+};
+
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v1.e[2]);
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v1.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v1.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v1.e[2]);
+}
+
+#endif
