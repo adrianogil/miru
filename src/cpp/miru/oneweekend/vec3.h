3565f52abbcc65767f656f8d51fe9ae53911fb73
Adriano Gil <adrianogil.san@gmail.com>
2019-07-26 06:58:28 -0400

    [RaytracingInOneWeekend] Rendered a sphere

--- src/cpp/miru/oneweekend/vec3.h
+++ src/cpp/miru/oneweekend/vec3.h
@@ -41,15 +41,15 @@ inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
 }
 
 inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v1.e[2]);
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
 
 inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v1.e[2]);
+    return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
 }
 
 inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v1.e[2]);
+    return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
 inline vec3 operator*(const float f, const vec3 &v1) {
