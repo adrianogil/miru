8b2556d480617146d3787e473d84da9de3582ce1
Adriano Gil <adrianogil.san@gmail.com>
2019-07-26 07:23:52 -0400

    [RaytracingInOneWeekend] Let's show sphere normals

--- src/cpp/miru/oneweekend/main.cpp
+++ src/cpp/miru/oneweekend/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
@@ -9,17 +9,25 @@ bool hit_sphere(const vec3& center, float radius, const ray& r)
     float c = dot(oc, oc) - radius * radius;
     float discriminant = b * b - 4 * a * c;
 
-    return (discriminant > 0);
+    if (discriminant < 0)
+    {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 vec3 color(const ray& r)
 {
-    if (hit_sphere(vec3(0,0,-1), 0.5, r))
+    float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+    // return vec3(t, 0, 0);
+    if (t > 0.0)
     {
-        return vec3(1, 0, 0);
+        vec3 N = unit_vector(r.point_at(t) - vec3(0, 0, -1));
+        return 0.5 * (N + 1.0);
     }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
 
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
--- src/cpp/miru/oneweekend/vec3.h
+++ src/cpp/miru/oneweekend/vec3.h
@@ -52,12 +52,16 @@ inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
+inline vec3 operator+(const vec3 &v1, float f) {
+    return vec3(v1.e[0] + f, v1.e[1] + f, v1.e[2] + f);
+}
+
 inline vec3 operator*(const float f, const vec3 &v1) {
     return vec3(v1.e[0] * f, v1.e[1] * f, v1.e[2] * f);
 }
 
 inline float dot(const vec3 &v1, const vec3 &v2) {
-    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v1.e[2];
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
 
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
