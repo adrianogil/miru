9f46dd6b396e41f90f345aff28cf19a51272576d
Adriano Gil <adrianogil.san@gmail.com>
2019-07-25 20:46:10 -0400

    Fixed error on vec3 math

--- src/cpp/miru/oneweekend/vec3.h
+++ src/cpp/miru/oneweekend/vec3.h
@@ -37,7 +37,7 @@ public:
 
 
 inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v1.e[2]);
+    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
 
 inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
