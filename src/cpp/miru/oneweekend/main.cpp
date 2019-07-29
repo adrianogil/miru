411d928dc003c3d128200f3236216b47a1503ba0
Adriano Gil <adrianogil.san@gmail.com>
2019-07-28 21:46:48 -0400

    [RaytracingInOneWeekend] Added diffuse material

--- src/cpp/miru/oneweekend/main.cpp
+++ src/cpp/miru/oneweekend/main.cpp
@@ -4,13 +4,27 @@
 #include "float.h"
 #include "camera.h"
 
+vec3 random_in_unit_sphere()
+{
+    vec3 p;
+    do
+    {
+        p = 2.0 * get_random_vec3() - get_vec3_one();
+
+    } while(dot(p,p) >= 1.0);
+
+    return p;
+}
+
 vec3 color(const ray& r, hitable *world)
 {
     hit_record rec;
 
     if (world->hit(r, 0.0, MAXFLOAT, rec))
     {
-        return 0.5 * (rec.normal + 1.0);
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+
+        return 0.5 * color( ray(rec.p, target - rec.p), world);
         // Show only a red color
         // return vec3(1, 0, 0);
     }
@@ -29,10 +43,10 @@ int main()
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    int obj_list_size = 1;
+    int obj_list_size = 2;
     hitable *list[obj_list_size];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    // list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
 
     hitable *world = new hitable_list(list, obj_list_size);
 
@@ -53,6 +67,8 @@ int main()
             }
 
             col /= float(ns);
+
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
--- src/cpp/miru/oneweekend/vec3.h
+++ src/cpp/miru/oneweekend/vec3.h
@@ -43,6 +43,18 @@ public:
     float e[3];
 };
 
+namespace {
+    inline vec3 get_random_vec3()
+    {
+        return vec3(drand48(), drand48(), drand48());
+    }
+
+    inline vec3 get_vec3_one()
+    {
+        return vec3(1.0, 1.0, 1.0);
+    }
+}
+
 
 inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
