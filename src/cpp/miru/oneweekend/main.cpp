f9720a401d9277ceeee9418a40ee776e4a06dc89
Adriano Gil <adrianogil.san@gmail.com>
2019-07-28 21:11:46 -0400

    [RaytracingInOneWeekend] Added antialiasing step

--- src/cpp/miru/oneweekend/main.cpp
+++ src/cpp/miru/oneweekend/main.cpp
@@ -2,7 +2,7 @@
 #include "sphere.h"
 #include "hitablelist.h"
 #include "float.h"
-
+#include "camera.h"
 
 vec3 color(const ray& r, hitable *world)
 {
@@ -25,13 +25,9 @@ int main()
 
     int nx = 200;
     int ny = 100;
+    int ns = 100;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
 
     int obj_list_size = 1;
     hitable *list[obj_list_size];
@@ -40,15 +36,23 @@ int main()
 
     hitable *world = new hitable_list(list, obj_list_size);
 
+    camera cam;
+
     for (int y = ny - 1; y >= 0; y--)
     {
         for (int x = 0; x < nx; x++)
         {
-            float u = float(x) / float(nx);
-            float v = float(y) / float(ny);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++)
+            {
+                float u = float(x + drand48()) / float(nx);
+                float v = float(y + drand48()) / float(ny);
 
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            vec3 col = color(r, world);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world);
+            }
+
+            col /= float(ns);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
--- src/cpp/miru/oneweekend/vec3.h
+++ src/cpp/miru/oneweekend/vec3.h
@@ -22,12 +22,20 @@ public:
     inline float operator[](int i) const { return e[i]; }
     inline float& operator[](int i) { return e[i]; };
 
-    inline vec3& operator+=(const vec3 &v2);
+    inline vec3& operator+=(const vec3 &v2)
+    {
+        e[0] += v2.e[0]; e[1] += v2.e[1]; e[2] += v2.e[2];
+        return *this;
+    }
     inline vec3& operator-=(const vec3 &v2);
     inline vec3& operator*=(const vec3 &v2);
     inline vec3& operator/=(const vec3 &v2);
     inline vec3& operator*=(const float t);
-    inline vec3& operator/=(const float t);
+    inline vec3& operator/=(const float t)
+    {
+        e[0] /= t; e[1] /= t; e[2] /= t;
+        return *this;
+    }
 
     inline float length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
     inline float squared_length() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
